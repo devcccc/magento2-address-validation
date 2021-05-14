024d55618b22dab35245eb58de8d677de9b2c9ff
4ctobias <tobias@4c.media>
2021-05-14 08:47:19 +0200

    Implemented JavaScript-debug-logging

--- view/frontend/web/js/helper/logger.js
+++ view/frontend/web/js/helper/logger.js
@@ -5,12 +5,15 @@ define([
 ], function ($) {
     'use strict';
 
-    function logData(data) {
-        var shouldLog =  window.checkoutConfig.cccc.addressvalidation.endereco &&  window.checkoutConfig.cccc.addressvalidation.endereco.development
-            && window.checkoutConfig.cccc.addressvalidation.endereco.development.javascript_debug;
+    return {
+        logData: function(data)
+        {
+            var shouldLog = window.checkoutConfig.cccc.addressvalidation.endereco && window.checkoutConfig.cccc.addressvalidation.endereco.development
+                && window.checkoutConfig.cccc.addressvalidation.endereco.development.javascript_debug;
 
-        if (shouldLog) {
-            console.debug(data);
+            if (shouldLog) {
+                console.debug(data);
+            }
         }
-    }
+    };
 });
